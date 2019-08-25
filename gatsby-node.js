6941a4311135c922cb3a03942da12d7f4d6eac02
Charbel Chahine <chahine.charbel@hotmail.com>
2019-08-24 22:59:50 -0400

    more linting

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,43 +1,42 @@
 /**
  * Gatsby's Node APIs.
  * https://www.gatsbyjs.org/docs/node-apis/
- **/
+ * */
 
-const path = require('path')
-const i18n = require('./src/i18n/config/i18n')
+const path = require('path');
+const i18n = require('./src/i18n/config/i18n');
 
-const _ = require(`lodash`)
-const Promise = require(`bluebird`)
-const slash = require(`slash`)
+const Promise = require(`bluebird`);
 
 exports.onCreatePage = ({ page, actions }) => {
-	if (page.componentPath && page.componentPath.match(/pages|templates/)) {
-		const { createPage, deletePage } = actions
-		const getTitle = (object, path) => {
-			var array = path.split('/').filter(val => val)
-			if (path === '/') {
-				array.push('home')
-			}
-			let value = {}
-			value = array.reduce(
-				(obj, key) => (obj && obj[key] !== 'undefined' ? obj[key] : undefined),
-				object
-			)
-			return value ? value.title : 'Untitled'
-		}
-		return new Promise(resolve => {
-			deletePage(page)
-			Object.keys(i18n).map(key => {
-				return createPage({
-					...page,
-					path: i18n[key].path + page.path,
-					context: {
-						lang: i18n[key],
-						title: getTitle(i18n[key], page.path)
-					}
-				})
-			})
-			resolve()
-		})
-	}
-}
+    if (page.componentPath && page.componentPath.match(/pages|templates/)) {
+        const { createPage, deletePage } = actions;
+        const getTitle = (object, path) => {
+            const array = path.split('/').filter(val => val);
+            if (path === '/') {
+                array.push('home');
+            }
+            let value = {};
+            value = array.reduce(
+                (obj, key) => (obj && obj[key] !== 'undefined' ? obj[key] : undefined),
+                object,
+            );
+            return value ? value.title : 'Untitled';
+        };
+        return new Promise(resolve => {
+            deletePage(page);
+            Object.keys(i18n).map(key => {
+                return createPage({
+                    ...page,
+                    path: i18n[key].path + page.path,
+                    context: {
+                        lang: i18n[key],
+                        title: getTitle(i18n[key], page.path),
+                    },
+                });
+            });
+            resolve();
+        });
+    }
+    return null;
+};
