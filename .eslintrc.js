03a8322f9cc34e05aed62831d52b405caee3b9b6
Charbel Chahine <chahine.charbel@hotmail.com>
2019-08-24 21:01:01 -0400

    update packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,24 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+    env: {
+        browser: true,
+        node: true,
+        es6: true,
     },
-    "extends": [
-        "airbnb",
-        "prettier",
-        "prettier/react"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+    extends: ['airbnb', 'prettier', 'prettier/react'],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
     },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
         },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+        ecmaVersion: 2018,
+        sourceType: 'module',
     },
-    "plugins": [
-        "react",
-        'jsx-a11y',
-        "prettier"
-    ],
-    "rules": {
-    }
+    plugins: ['react', 'jsx-a11y', 'prettier'],
+    rules: {
+        'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+        'react/prefer-stateless-function': [1, { ignorePureComponents: true }],
+    },
 };
