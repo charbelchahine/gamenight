81b3f275ba205d8976a2c8ff137805ccc3dda51f
Charbel Chahine <chahine.charbel@hotmail.com>
2019-08-25 16:24:52 -0400

    Turn nav into function

--- src/components/navlinks.js
+++ src/components/navlinks.js
@@ -3,22 +3,20 @@ import T from 'i18n-react';
 import { Button } from 'react-md';
 import Link from './link';
 
-class NavLinks extends React.Component {
-    render() {
-        return (
-            <div className="navLinks">
-                <Link to="/">
-                    <Button flat>{T.translate('home.title')}</Button>
-                </Link>
-                <Link to="/projects">
-                    <Button flat>Projects</Button>
-                </Link>
-                <Link to="/contact">
-                    <Button flat>{T.translate('contact.title')}</Button>
-                </Link>
-            </div>
-        );
-    }
+function NavLinks() {
+    return (
+        <div className="navLinks">
+            <Link to="/">
+                <Button flat>{T.translate('home.title')}</Button>
+            </Link>
+            <Link to="/projects">
+                <Button flat>Projects</Button>
+            </Link>
+            <Link to="/contact">
+                <Button flat>{T.translate('contact.title')}</Button>
+            </Link>
+        </div>
+    );
 }
 
 export default NavLinks;
