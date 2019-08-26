98439cec86e387835f6500a266e3c406af99b853
Charbel Chahine <chahine.charbel@hotmail.com>
2019-08-26 00:46:11 -0400

    added sanitizer

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,14 +1,18 @@
+/* eslint-disable react/no-danger */
 import React from 'react';
 import PropTypes from 'prop-types';
 import T from 'i18n-react';
 import Helmet from 'react-helmet';
+import dompurify from 'dompurify';
 import Layout from '../components/layout';
+
+const sanitizer = dompurify.sanitize;
 
 const PageTemplate = ({ pageContext: { lang, html, title }, location: { pathname } }) => (
     <Layout path={pathname}>
         {T.setTexts(lang)}
         <Helmet title={title} />
-        <div dangerouslySetInnerHTML={{ __html: html }} />
+        <div dangerouslySetInnerHTML={{ __html: sanitizer(html) }} />
     </Layout>
 );
 
